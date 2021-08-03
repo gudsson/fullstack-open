b0766ca240cadbd4f7c912d6c93b295e288f9677
Jay Gudsson <gudsson@gmail.com>
2021-08-03 14:31:30 -0700

    4.6 complete

--- part4/blog-app/utils/list_helper.js
+++ part4/blog-app/utils/list_helper.js
@@ -9,7 +9,14 @@ const totalLikes = (blogs) => {
     : blogs.reduce((a, c) => a + c.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {}
+  
+  return blogs.reduce((a, c) => a.likes > c.likes ? a : c)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
