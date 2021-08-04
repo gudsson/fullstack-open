080abfcf4ec1bd66f94fe400dea8375ff8b2b703
Jay Gudsson <gudsson@gmail.com>
2021-08-04 08:41:57 -0700

    4.19 complete

--- part4/blog-app/utils/middleware.js
+++ part4/blog-app/utils/middleware.js
@@ -21,8 +21,9 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({
-    error: 'invalid token'
-  })
+      error: 'invalid token'
+    })
+  }
 
   logger.error(error.message)
 
