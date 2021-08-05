16d4c387b06cabe122d535487299d0da945dd86f
Jay Gudsson <gudsson@gmail.com>
2021-08-04 18:28:43 -0700

    5.1 complete

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -17,44 +17,6 @@ const App = () => {
     )  
   }, [])
 
-  
-
-  // const loginForm = () => (
-  //   <>
-  //     <h2>login to application</h2>
-  //     <form onSubmit={handleLogin}>
-  //       <div>
-  //         username
-  //           <input
-  //           type="text"
-  //           value={username}
-  //           name="Username"
-  //           onChange={({ target }) => setUsername(target.value)}
-  //         />
-  //       </div>
-  //       <div>
-  //         password
-  //           <input
-  //           type="password"
-  //           value={password}
-  //           name="Password"
-  //           onChange={({ target }) => setPassword(target.value)}
-  //         />
-  //       </div>
-  //       <button type="submit">login</button>
-  //     </form>
-  //   </>    
-  // )
-
-  // const blogsForm = () => (
-  //   <div>
-  //     <h2>blogs</h2>
-  //     {blogs.map(blog =>
-  //       <Blog key={blog.id} blog={blog} />
-  //     )}
-  //   </div>
-  // )
-
   if (user === null) {
     <Notification message={errorMessage} />
 
