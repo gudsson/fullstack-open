720746a32550465477921b33931a4aba1bcf86c2
Jay Gudsson <gudsson@gmail.com>
2021-08-06 09:33:26 -0700

    6.10 complete

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
-  // filter: filterReducer
+  notification: notificationReducer
 })
 
 const store = createStore(reducer, composeWithDevTools())
