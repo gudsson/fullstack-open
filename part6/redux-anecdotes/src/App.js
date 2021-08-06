f5b86c240e5638b431b5d79b960e19ac99f87acb
Jay Gudsson <gudsson@gmail.com>
2021-08-06 12:05:32 -0700

    6.12 complete

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,17 +3,9 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
-import { useDispatch } from 'react-redux'
-import { filterChange } from './reducers/filterReducer'
+
 
 const App = () => {
-  const dispatch = useDispatch()
-
-  const filterSelected = event => {
-    console.log(event.target.value)
-    dispatch(filterChange(event.target.value))
-  }
-
   return (
     <div>
       <Notification />
